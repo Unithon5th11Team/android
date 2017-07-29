9e5c8e42c4abed275f6bb49f6a4fd77f61f7847a
eun-bi <uko02111@naver.com>
2017-07-29 19:14:07 +0900

    연극 세부설명 페이지 (아직)

---
+++ app/src/main/java/kth/com/unithon11team/PlayDetailActivity.java
@@ -0,0 +1,31 @@
+package kth.com.unithon11team;
+
+
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import kth.com.unithon11team.utils.DepthActivity;
+
+public class PlayDetailActivity extends DepthActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_detail);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(" ");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+
+        initView();
+    }
+
+
+}
+private void initView() {
+    }
