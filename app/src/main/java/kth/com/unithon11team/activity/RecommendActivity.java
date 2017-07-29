9300cfd1481a48ccc52f8592459e3f428f47b0e3
nanamare <cordorshs@gmail.com>
2017-07-30 08:36:56 +0900

    [뻘커밋]

---
+++ app/src/main/java/kth/com/unithon11team/activity/RecommendActivity.java
@@ -0,0 +1,15 @@
+package kth.com.unithon11team.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import kth.com.unithon11team.R;
+
+public class RecommendActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_recommend);
+	}
+}
