a66ec56af222dc86bcff8d032d220e14fd85e6d5
LikeJust <diddydwns135@gmail.com>
2017-07-30 08:28:34 +0900

    메인 탭 키워드 & 카메라 result 화면

--- app/src/main/java/kth/com/unithon11team/activity/ResultActivity.java
+++ app/src/main/java/kth/com/unithon11team/activity/ResultActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatImageView;
 import android.support.v7.widget.AppCompatTextView;
+import android.util.Log;
 
 import com.vocketlist.android.roboguice.log.Ln;
 
@@ -32,111 +33,110 @@ import rx.android.schedulers.AndroidSchedulers;
 
 public class ResultActivity extends AppCompatActivity {
 
-	@BindView(R.id.activity_result_circleIv)
-	CircleImageView circleImageView;
-	@BindView(R.id.activity_result_emotion_tv1)
-	AppCompatTextView emotionTv1;
-	@BindView(R.id.activity_result_emotion_tv2)
-	AppCompatTextView emotionTv2;
-	@BindView(R.id.activity_result_emotion_tv3)
-	AppCompatTextView emotionTv3;
-	@BindView(R.id.activity_result_done_iv)
-	AppCompatImageView doneIv;
+    @BindView(R.id.activity_result_circleIv)
+    CircleImageView circleImageView;
+    @BindView(R.id.activity_result_emotion_tv1)
+    AppCompatTextView emotionTv1;
+    @BindView(R.id.activity_result_emotion_tv2)
+    AppCompatTextView emotionTv2;
+    @BindView(R.id.activity_result_emotion_tv3)
+    AppCompatTextView emotionTv3;
+    @BindView(R.id.activity_result_done_iv)
+    AppCompatImageView doneIv;
 
 
-	public static final String REQUEST_IMAGE_FROM_CAMERA = "request_image_from_camera";
-	public static final String TAG = "ResultActivity";
-	public static final String REKOGNATION_IMAGE = "rekognation_image";
+    public static final String REQUEST_IMAGE_FROM_CAMERA = "request_image_from_camera";
+    public static final String TAG = "ResultActivity";
+    public static final String REKOGNATION_IMAGE = "rekognation_image";
 
-	private String mPriortyType = "happy";
-	private Bitmap mBitmap = null;
+    private String mPriortyType = "happy";
+    private Bitmap mBitmap = null;
 
-	private List<Musical> musicalList;
-
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_result);
-		ButterKnife.bind(this);
-
-		Intent intent = getIntent();
-		Bundle args = intent.getExtras();
-		if (intent != null && args != null) {
-			//load to album mode
-			decodePicData(args);
-			decodeRecognationData(args);
-		} else {
-			emotionTv1.setText(getString(R.string.empty_emotion_title));
-			emotionTv2.setText(getString(R.string.empty_emotion_again));
-		}
+    private List<Musical> musicalList;
 
 
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+        ButterKnife.bind(this);
 
-	private void decodeRecognationData(Bundle args) {
-		if(args.getSerializable(REKOGNATION_IMAGE) != null ) {
-			List<RecognationImage> data = (List<RecognationImage>) args.getSerializable(REKOGNATION_IMAGE);
-			setRecognationData(data);
-		}
-	}
-
-	private void setRecognationData(List<RecognationImage> data) {
-		if(data.size() != 0) {
-			mPriortyType = data.get(0).type;
-			emotionTv1.setText(String.format(getString(R.string.emotion_title), data.get(0).confidence, data.get(0).type));
-			emotionTv2.setText(String.format(getString(R.string.emotion_title), data.get(1).confidence, data.get(1).type));
-			emotionTv3.setText(String.format(getString(R.string.emotion_title), data.get(2).confidence, data.get(2).type));
-		}
-
-	}
-
-	private void decodePicData(Bundle args) {
-		Bitmap bitmap = null;
-		String imgPath = args.getString(REQUEST_IMAGE_FROM_CAMERA);
-		Uri imgUri = Uri.parse(imgPath);
-
-		try {
-			bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imgUri);
-		} catch (IOException exception) {
-			Ln.d(TAG, exception);
-		}
-
-		ByteArrayOutputStream stream = new ByteArrayOutputStream();
-		bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
-		byte[] byteArray = stream.toByteArray();
-
-		mBitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+        Intent intent = getIntent();
+        Bundle args = intent.getExtras();
+        if (intent != null && args != null) {
+            //load to album mode
+            decodePicData(args);
+            decodeRecognationData(args);
+        } else {
+            emotionTv1.setText(getString(R.string.empty_emotion_title));
+            emotionTv2.setText(getString(R.string.empty_emotion_again));
+        }
 
 
-		circleImageView.setImageBitmap(mBitmap);
+    }
 
-	}
+    private void decodeRecognationData(Bundle args) {
+        if (args.getSerializable(REKOGNATION_IMAGE) != null) {
+            List<RecognationImage> data = (List<RecognationImage>) args.getSerializable(REKOGNATION_IMAGE);
+            setRecognationData(data);
+        }
+    }
 
-	@OnClick(R.id.activity_result_done_iv)
-	void onDoneClick(){
-		sendToServerEmotion();
-	}
+    private void setRecognationData(List<RecognationImage> data) {
+        if (data.size() != 0) {
+            mPriortyType = data.get(0).type;
+            emotionTv1.setText(String.format(getString(R.string.emotion_title), data.get(0).confidence, data.get(0).type));
+            emotionTv2.setText(String.format(getString(R.string.emotion_title), data.get(1).confidence, data.get(1).type));
+            emotionTv3.setText(String.format(getString(R.string.emotion_title), data.get(2).confidence, data.get(2).type));
+        }
+    }
 
-	private void sendToServerEmotion() {
-		MusicalServiceManager.sendEmotionToServer(mPriortyType)
-				.observeOn(AndroidSchedulers.mainThread())
-				.subscribe(new Subscriber<Response<BaseResponse<Result>>>() {
-					@Override
-					public void onCompleted() {
+    private void decodePicData(Bundle args) {
+        Bitmap bitmap = null;
+        String imgPath = args.getString(REQUEST_IMAGE_FROM_CAMERA);
+        Uri imgUri = Uri.parse(imgPath);
 
-					}
+        try {
+            bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imgUri);
+        } catch (IOException exception) {
+            Ln.d(TAG, exception);
+        }
 
-					@Override
-					public void onError(Throwable e) {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+        byte[] byteArray = stream.toByteArray();
 
-					}
+        mBitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
 
-					@Override
-					public void onNext(Response<BaseResponse<Result>> baseResponseResponse) {
-						musicalList = baseResponseResponse.body().mResult.musicalList;
-					}
-				});
-	}
+
+        circleImageView.setImageBitmap(mBitmap);
+
+    }
+
+    @OnClick(R.id.activity_result_done_iv)
+    void onDoneClick() {
+        sendToServerEmotion();
+    }
+
+    private void sendToServerEmotion() {
+        MusicalServiceManager.sendEmotionToServer(mPriortyType)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<Response<BaseResponse<Result>>>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(Response<BaseResponse<Result>> baseResponseResponse) {
+                        musicalList = baseResponseResponse.body().mResult.musicalList;
+                    }
+                });
+    }
 
 }
--- app/src/main/java/kth/com/unithon11team/category/CategoryCategory.java
+++ app/src/main/java/kth/com/unithon11team/category/CategoryCategory.java
@@ -9,17 +9,23 @@ import kth.com.unithon11team.R;
  */
 
 public enum CategoryCategory {
-	All(R.string.category_all),
-	HORROR(R.string.category_horror);
+    All(R.string.category_all),
+    HORROR(R.string.category_horror),
+    HAPPY(R.string.category_happy),
+    SAD(R.string.category_sad),
+    ANGRY(R.string.category_angry),
+    CONFUSED(R.string.category_confused),
+    DISGUSTED(R.string.category_disgusted),
+    SURPRISED(R.string.category_surprised),
+    CALM(R.string.category_calm);
 
+    private final int resId;
 
-	private final int resId;
+    CategoryCategory(@StringRes int id) {
+        this.resId = id;
+    }
 
-	CategoryCategory(@StringRes int id) {
-		this.resId = id;
-	}
-
-	public int getResId() {
-		return resId;
-	}
+    public int getResId() {
+        return resId;
+    }
 }
